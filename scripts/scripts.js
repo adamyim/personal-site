15f4deaac3bcb916fa5b75aeae40dfe91ff8de7e
Adam Yim <adam.yim@gmail.com>
2021-03-11 12:21:20 -0500

    text, YT embed, better scroll targeting
    - changed js script so it scrolls to the same place consistently, regardless of screen size
    - added work history text, uploaded resume
    - added a responsive YT embed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,16 @@
 $('.test, .nav-link, .navbar-brand, .new-button, .dropdown-item, .top-content').click(function() {
     var sectionTo = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top
+    var navHeight = $('.navbar').outerHeight();
+    var winwid = $(window).width();
+    
+if(winwid <= 575) {
+	$('html, body').animate({
+      scrollTop: $(sectionTo).offset().top-navHeight+72
     }, 500);
+	return false;
+}
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top+15
+    }, 500);
+    return false;
 });
